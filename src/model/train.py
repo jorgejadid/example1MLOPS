aae889679ab7cf3f2013c95c4907362c376f0c71
jorgejadid <166310418+jorgejadid@users.noreply.github.com>
2024-04-07 17:26:29 +0300

    Update split_data

--- src/model/train.py
+++ src/model/train.py
@@ -6,6 +6,7 @@ import os
 
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 
@@ -34,6 +35,8 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df)
+    return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
